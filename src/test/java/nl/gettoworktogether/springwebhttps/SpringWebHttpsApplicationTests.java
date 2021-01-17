7c5d03ca59ffa293dd5312671da38ab9d6c0a8c9
Peter Anema <email@peteranema.nl>
2021-01-18 00:10:20 +0100

    Initial commit

---
+++ src/test/java/nl/gettoworktogether/springwebhttps/SpringWebHttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.gettoworktogether.springwebhttps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebHttpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
